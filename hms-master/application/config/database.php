09f04373ee903c5c7a03973110f129ba25a39b54
ablas123 <67266977+ablas123@users.noreply.github.com>
2026-04-03 13:31:42 +0200

    Update database.php

--- hms-master/application/config/database.php
+++ hms-master/application/config/database.php
@@ -48,27 +48,24 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$ondevelop = TRUE;
+$ondevelop = FALSE;  // تم التغيير إلى FALSE للإنتاج
 
-$db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = '';
-$db['default']['database'] = 'hms';
+$db['default']['hostname'] = 'gateway01.eu-central-1.prod.aws.tidbcloud.com';
+$db['default']['port']     = '4000';
+$db['default']['username'] = '4Q3JgFwtV72XTzm.root';
+$db['default']['password'] = 'H35cOolgETH9RMa4';
+$db['default']['database'] = 'hms_db';
 $db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = '';
-$db['default']['pconnect'] = TRUE;
+$db['default']['pconnect'] = FALSE;   // يفضل FALSE مع قواعد البيانات السحابية
 $db['default']['db_debug'] = TRUE;
 $db['default']['cache_on'] = FALSE;
 $db['default']['cachedir'] = '';
-$db['default']['char_set'] = 'utf8';
-$db['default']['dbcollat'] = 'utf8_general_ci';
+$db['default']['char_set'] = 'utf8mb4';
+$db['default']['dbcollat'] = 'utf8mb4_general_ci';
 $db['default']['swap_pre'] = '';
 $db['default']['autoinit'] = TRUE;
 $db['default']['stricton'] = FALSE;
-
-
-
-
 
 /* End of file database.php */
 /* Location: ./application/config/database.php */
