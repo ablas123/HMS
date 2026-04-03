595defd28aa59b72f25133ae70bd331d6c091ac2
ablas123 <67266977+ablas123@users.noreply.github.com>
2026-04-03 18:00:31 +0200

    Update database.php

--- hms-master/application/config/database.php
+++ hms-master/application/config/database.php
@@ -1,14 +1,20 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
+/*
+| -------------------------------------------------------------------
+| DATABASE CONNECTIVITY SETTINGS
+| -------------------------------------------------------------------
+*/
+
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'mysql-3cb37202-ablas123a-f12a.a.aivencloud.com';
-$db['default']['port']     = '10126';
+$db['default']['dsn'] = 'mysql:host=mysql-3cb37202-ablas123a-f12a.a.aivencloud.com;port=10126;dbname=defaultdb;charset=utf8mb4;sslmode=require';
+$db['default']['hostname'] = '';
 $db['default']['username'] = 'avnadmin';
 $db['default']['password'] = 'AVNS_I0p4cP5M1bz4wkTKW2I';
-$db['default']['database'] = 'defaultdb';
-$db['default']['dbdriver'] = 'mysqli';
+$db['default']['database'] = '';
+$db['default']['dbdriver'] = 'pdo';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = FALSE;
 $db['default']['db_debug'] = TRUE;
@@ -19,5 +25,5 @@ $db['default']['dbcollat'] = 'utf8mb4_general_ci';
 $db['default']['swap_pre'] = '';
 $db['default']['autoinit'] = TRUE;
 $db['default']['stricton'] = FALSE;
-$db['default']['encrypt']  = TRUE;
-$db['default']['ssl_verify'] = FALSE;
+
+/* End of file database.php */
