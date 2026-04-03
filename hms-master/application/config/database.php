7fc61a6355a014176c0046e3087384492ce532b1
ablas123 <67266977+ablas123@users.noreply.github.com>
2026-04-03 19:13:15 +0200

    Update database.php

--- hms-master/application/config/database.php
+++ hms-master/application/config/database.php
@@ -1,25 +1,9 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-$active_group = 'default';
-$active_record = TRUE;
-
-$db['default']['hostname'] = 'gateway01.eu-central-1.prod.aws.tidbcloud.com';
-$db['default']['port']     = '4000';
-$db['default']['username'] = '4Q3JgFwtV72XTzm.root';
-$db['default']['password'] = 'H35cOolgETH9RMa4';
-$db['default']['database'] = 'hms_db';
+$db['default']['hostname'] = 'mysql-3cb37202-ablas123a-f12a.a.aivencloud.com';
+$db['default']['port']     = '10126';
+$db['default']['username'] = 'avnadmin';
+$db['default']['password'] = 'AVNS_I0p4cP5M1bz4wkTKW2I';
+$db['default']['database'] = 'defaultdb';
 $db['default']['dbdriver'] = 'mysqli';
-$db['default']['dbprefix'] = '';
-$db['default']['pconnect'] = FALSE;
-$db['default']['db_debug'] = TRUE;
-$db['default']['cache_on'] = FALSE;
-$db['default']['cachedir'] = '';
-$db['default']['char_set'] = 'utf8mb4';
-$db['default']['dbcollat'] = 'utf8mb4_general_ci';
-$db['default']['swap_pre'] = '';
-$db['default']['autoinit'] = TRUE;
 $db['default']['stricton'] = FALSE;
-$db['default']['encrypt'] = TRUE;
+$db['default']['encrypt'] = FALSE;
 $db['default']['ssl_verify'] = FALSE;
-
-/* End of file database.php */
